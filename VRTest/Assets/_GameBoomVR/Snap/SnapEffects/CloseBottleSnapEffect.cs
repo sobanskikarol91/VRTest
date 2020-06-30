9e38a9e7fd33785fbfe5b15f8a2e0b6b30f5dd9e
Karol Sobanski <sobanskikarol91@gmail.com>
2020-06-30 14:28:57 +0200

    Add SpinTime

--- VRTest/Assets/_GameBoomVR/Snap/SnapEffects/CloseBottleSnapEffect.cs
+++ VRTest/Assets/_GameBoomVR/Snap/SnapEffects/CloseBottleSnapEffect.cs
@@ -6,7 +6,8 @@ public class CloseBottleSnapEffect : GrabbableEvents, ISnapAreaEnter, ISnapAreaE
 {
     [SerializeField] Transform aboveCap;
     private Grabbable grabbable;
-
+    private readonly float prepareToSpinTime = .2f;
+    private readonly float spinTime = .5f;
 
     public void SnapEnter(Grabbable subject)
     {
@@ -23,8 +24,8 @@ public class CloseBottleSnapEffect : GrabbableEvents, ISnapAreaEnter, ISnapAreaE
     {
         Debug.Log("Move");
         grabbable.transform.SetParent(transform, true);
-        iTween.MoveTo(grabbable.gameObject, iTween.Hash("position", aboveCap.localPosition, "time", 0.4f, "islocal", true, "easetype", iTween.EaseType.easeInOutCirc));
-        iTween.RotateTo(grabbable.gameObject, iTween.Hash("rotation", new Vector3(0, 0, 90), "time", 0.4f, "easetype", iTween.EaseType.linear, "islocal", true));
+        iTween.MoveTo(grabbable.gameObject, iTween.Hash("position", aboveCap.localPosition, "time", prepareToSpinTime, "islocal", true, "easetype", iTween.EaseType.easeInOutCirc));
+        iTween.RotateTo(grabbable.gameObject, iTween.Hash("rotation", new Vector3(0, 0, 90), "time", prepareToSpinTime, "easetype", iTween.EaseType.linear, "islocal", true));
 
         grabbable.GetComponent<Rigidbody>().isKinematic = true;
         grabbable.GetComponent<Collider>().enabled = false;
@@ -45,8 +46,8 @@ public class CloseBottleSnapEffect : GrabbableEvents, ISnapAreaEnter, ISnapAreaE
     void SpinCap()
     {
         Debug.Log("Spin");
-        iTween.MoveTo(grabbable.gameObject, iTween.Hash("position", Vector3.zero, "time", 0.6f, "islocal", true));
-        iTween.RotateTo(grabbable.gameObject, iTween.Hash("rotation", new Vector3(180, 0, 90), "time", 0.6f, "easetype", iTween.EaseType.linear, "islocal", true));
+        iTween.MoveTo(grabbable.gameObject, iTween.Hash("position", Vector3.zero, "time", spinTime, "islocal", true));
+        iTween.RotateTo(grabbable.gameObject, iTween.Hash("rotation", new Vector3(180, 0, 90), "time", spinTime, "easetype", iTween.EaseType.linear, "islocal", true));
     }
 
     public void OnUnsnap(GrabbableEventArgs subject)
